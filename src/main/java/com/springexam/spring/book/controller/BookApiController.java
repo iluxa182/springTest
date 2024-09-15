0a231ef4b99edf170f3916118c81a1105e575075
ILUA BARANOV <iluxa182@yandex.ru>
2024-09-15 14:29:01 +0300

    restController

---
+++ src/main/java/com/springexam/spring/book/controller/BookApiController.java
@@ -0,0 +1,16 @@
+package com.springexam.spring.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class BookApiController {
+    @GetMapping("/")
+
+    public String ok(){
+
+        return "ok";
+    }
+
+}
